d17a53dfd8b283523632e75fbd05a52b2090e296
heroq <96118954+heroq@users.noreply.github.com>
2022-09-05 21:52:48 +0900

    socket.io install

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/:roomKey', function(req, res, next) {
+  res.render('index', { roomKey: req.params.roomKey });
+  var io = req.app.get('io');
+  io.on('connection', (socket) => {
+    socket.on('join', (roomKey, userId) => {
+      socket.join(roomKey);
+      socket.to(roomKey).brodcast.emit('joinRoom', userId); // sender 제외 소켓 전송
+  
+      socket.on('exit', () => {
+        socket.to(roomKey).broadcast.emit('exitRoom', userId);
+      })
+    })
+  })
 });
+
+
+
 
 module.exports = router;
